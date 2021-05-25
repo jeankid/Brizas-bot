833c2ab8eabfcaf7f772b5b3c2eaf0a651b716b8
jeankid <80468065+jeankid@users.noreply.github.com>
2021-05-25 19:54:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const ownerNumber = ["557187645787@s.whatsapp.net"] 
+const ownerNumber = ["558291099267@s.whatsapp.net"] 
 // mude somente o numero e não tire o @s.whatsapp.net
 
-const OriginalOwner = '557187645787' 
+const OriginalOwner = '558291099267' 
 // mude para seu numero
 
 var gimagenotregister = true
